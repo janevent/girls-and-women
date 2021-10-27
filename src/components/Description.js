406ca2d4273b234f04db69a29f139ebcbaba2b82
janeventer <janeanneventer@gmail.com>
2021-10-27 10:35:07 -0700

    add description page

---
+++ src/components/Description.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Description(){
+
+    [title] = useState("Donations go to KWECHIPA");
+    [paragraph1, updateParagraph1] = useState("text");
+    [paragraph2, updateParagraph2] = useState("text");
+    [image1, updateImage1] = useState("image");
+
+    return (
+        <div className="Description">
+            
+        </div>
+    )
+
+}
