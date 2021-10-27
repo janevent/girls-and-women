dff74c445a7b817a2cc68a993ef88ece53c975ec
janeventer <janeanneventer@gmail.com>
2021-10-27 16:11:47 -0700

    Add a Thank You message and push to description page

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,18 +1,22 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Amount from './Amount';
 
-export default function Donate(){
-    const [amount, setAmount] = useState(5);
-
+export default function Donate(props){
+    const [amount, setAmount] = useState(20);
+    let history = useHistory()
     const onSubmitAmount = (event) => {
         event.preventDefault();
         console.log('onSubmitAmount', amount)
+        alert("Thank you for your Donation");
+        history.push('/description');
     }
 
     const onIncreaseAmount = () => {
         setAmount(amount+5)
     }
+
     const onDecreaseAmount = () => {
         if(amount>5){
             setAmount(amount-5)
