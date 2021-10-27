85c0ec5f65670ac37118141a264b33f80cbdc327
janeventer <janeanneventer@gmail.com>
2021-10-27 10:18:23 -0700

    add Navigation Bar with links

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { link } from 'react-router-dom';
+
+export default function NavigationBar(){
+
+    return(
+        <div>
+            <link path="/description" ></link>
+            <link path="/donate"></link>
+        </div>
+    )
+}
