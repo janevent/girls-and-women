c9aa4cae5d896b4bb367a7458c2c13f466632baf
janeventer <janeanneventer@gmail.com>
2021-11-25 11:38:40 -0800

    set a diasblePayment attribute, and hidePostalCode is set to true

--- src/components/BillingDetails.js
+++ src/components/BillingDetails.js
@@ -2,7 +2,9 @@ import { CardElement } from '@stripe/react-stripe-js';
 import React from 'react';
 import { useState } from 'react';
 
-export default function BillingDetails({name, email, onChangeInput}){
+export default function BillingDetails({billingDetails, onChangeInput}){
+
+    const {name, email} = billingDetails
 
     const cardElOptions = {
         style: {
@@ -10,14 +12,14 @@ export default function BillingDetails({name, email, onChangeInput}){
                 color: "darkred",
                 iconColor: "darkred"
             },
-            hidePostalCode: true
-        }
+        },
+        hidePostalCode: true
     }
     return(
         <div className="BillingDetails">
-            <input onChange={onChangeInput} name="name" value={name}/>
-            <input onChange={onChangeInput} name="email" value={email}/>
-            <CardElement options={cardElOptions} /> 
+            <input onChange={onChangeInput} type="text"name="name" placeholder="Full Name" value={name}/>
+            <input onChange={onChangeInput} type="text" placeholder="Email" name="email" value={email}/>
+            <CardElement options={cardElOptions} onChange={onChangeInput} /> 
         </div>
     )
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,32 +1,43 @@
 import BillingDetails from './BillingDetails';
-import { useElements } from '@stripe/react-stripe-js';
+import { useElements, Elements, CardElement } from '@stripe/react-stripe-js';
 
 import { useState } from 'react';
 
 export default function Modal( {display, handleClose, children}){
 
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
+    const [billingDetails, setBillingDetails] = useState(
+    {name: "", email: ""} )
+    
+    const [paymentDisabled, setPaymentDisabled] = useState(true);
 
     const stripeElements = useElements();
 
     const displayOrNotDisplay = display ? "Modal display-block" : "Modal display-none"
 
     const handleOnChangeInput = (event) => {
-        const { name, value } = event.target
-        console.log('name', name, 'value', value)
-        if(name==="name"){
-            setName(value)
-        }else if(name==="email"){
-            setName(value)
-        }
+
+        let target = event.target
+        
+        target && setBillingDetails( { ...billingDetails, [target.name]: target.value} )
+
+        let cardDetails = stripeElements.getElement(CardElement)
+        console.log(cardDetails)
+
+        Object.values(billingDetails).some( (deet) => deet === "") || cardDetails._invalid ? setPaymentDisabled(true) : setPaymentDisabled(false)
+        //_invalid
+        //_empty
+    }
+
+    const handlePaymentDisabled = (event) => {
+
     }
 
     return(
         <div className={displayOrNotDisplay}>
+            <button className="closeModal" onClick={handleClose}>Close</button>
             <h2>Payment Details</h2>
-            <BillingDetails name={name} email={email} onChangeInput={handleOnChangeInput} />
-            <button onClick={handleClose}>Close</button>
+            <BillingDetails billingDetails={billingDetails} onChangeInput={handleOnChangeInput} />
+            
         </div>
     )
 }
